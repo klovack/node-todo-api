7f9c9e130efcde6f11dce667d83fc8905614724c
Muhammad Rizki Fikriansyah <rizki.fikriansyah@hotmail.com>
2018-01-08 00:26:26 +0100

    Setup separate test and devs envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connection URL
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const url = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`${url}`);
