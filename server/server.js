39b559f8f4a640557be1db03836e45ccb21d1a41
Muhammad Rizki Fikriansyah <rizki.fikriansyah@hotmail.com>
2018-01-07 23:36:49 +0100

    Add tests for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send();
       }
 
-      return res.status(200).send(todo);
+      return res.status(200).send({ todo });
     }).catch((e) => {
       return res.status(400).send();
     });
