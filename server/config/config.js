d1c52a9bad15293f16c91fc59a10a1a1ee7aca83
Muhammad Rizki Fikriansyah <rizki.fikriansyah@hotmail.com>
2018-01-19 10:25:28 +0100

    bug fix

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
-const conf = require('./config.json');
-
 const env = process.env.NODE_ENV || 'development';
 
 console.log(`******************* env = ${env} *******************`);
 
 if (env === 'development' || env === 'test') {
+  // eslint-disable-next-line global-require
+  const conf = require('./config.json');
   const envConf = conf[env];
 
   Object.keys(envConf).forEach((key) => {
