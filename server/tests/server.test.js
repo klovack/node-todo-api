59aaefda0a443584503e7c9dbbdc46008fa17764
Muhammad Rizki Fikriansyah <rizki.fikriansyah@hotmail.com>
2018-01-19 01:57:17 +0100

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -303,3 +303,32 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove x-auth token on logout', (done) => {
+    // DELETE /users/me/token
+    // Set x-auth equal to token
+    // 200
+    // Find user, verify token is zero
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect((res.headers['x-auth'])).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id)
+          .then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
